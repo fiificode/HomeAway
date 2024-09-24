4c87518a452d0bb71faed1e575a3a74b7d514396
fiificode <fiificode@gmail.com>
2024-09-24 23:56:00 +0000

    Chore:Added Custom Splash screen

--- HomeAway/ContentView.swift
+++ HomeAway/ContentView.swift
@@ -8,16 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSplashScreen: Bool = true
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            if showSplashScreen {
+                SplashScreen()
+                    .transition(CustomTransition(isHome: false))
+            } else {
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.tint)
+                    Text("Hello, world!")
+                }
+                .transition(CustomTransition(isHome: true))
+                .padding()
+            }
         }
-        .padding()
+        .frame(maxWidth: .infinity,maxHeight: .infinity)
+//        .background(.appBlack)
+        .ignoresSafeArea()
+        .task {
+            guard showSplashScreen else { return }
+            try? await Task.sleep(for: .seconds(0.5))
+            withAnimation(.smooth(duration: (0.55))) {
+                showSplashScreen = false
+            }
+        }
     }
 }
+
+
+struct CustomTransition: Transition {
+    var isHome: Bool
+    func body(content: Content, phase: TransitionPhase) -> some View {
+        content.offset(y:phase.isIdentity ? 0 : isHome ? screenSize.height: -screenSize.height)
+    }
+    
+    var screenSize: CGSize {
+        if let screnSize = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.screen.bounds.size {
+            return screnSize
+        }
+        
+        return .zero
+    }
+}
+
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.appBlack)
+            Image("AppLogo3")
+        }.ignoresSafeArea()
+    }
+}
+
+
 
 #Preview {
     ContentView()
