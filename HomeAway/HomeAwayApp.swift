32ea21c0f48ba0e7a46fddfd64b384d94625e87c
fiificode <fiificode@gmail.com>
2024-09-24 21:16:49 +0000

    Initial Commit

---
+++ HomeAway/HomeAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAwayApp.swift
+//  HomeAway
+//
+//  Created by fiififranklin on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct HomeAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
